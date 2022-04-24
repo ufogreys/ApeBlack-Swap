64c016b1cf6c5f382bfacb98bc19cc990efa5306
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-04-24 03:36:52 -0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -82,26 +82,22 @@ export const LRT = new WrappedTokenInfo(
     logoURI:"https://cz.cash/images/tokens/LRT.png"
   },[]
 )
+export const APBK = new Token(
+  ChainId.MAINNET,
+  '0x3705496eE4614f7fE939E410f5A03dF988148e50',
+  9,
+  'APBK',
+  'ApeBlack'
+)
 
 export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.MAINNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
 export const BTCB = new Token(ChainId.MAINNET, '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c', 18, 'BTCB', 'Binance BTC')
 export const USDT = new Token(ChainId.MAINNET, '0x55d398326f99059fF775485246999027B3197955', 18, 'USDT', 'Tether USD')
-export const UST = new Token(
-  ChainId.MAINNET,
-  '0x23396cF899Ca06c4472205fC903bDB4de249D6fC',
-  18,
-  'UST',
-  'Wrapped UST Token'
-)
-export const ETH = new Token(
-  ChainId.MAINNET,
-  '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
-  18,
-  'ETH',
-  'Binance-Peg Ethereum Token'
-)
+export const UST = new Token(ChainId.MAINNET, '0x23396cF899Ca06c4472205fC903bDB4de249D6fC',  18,  'UST', 'Wrapped UST Token')
+export const ETH = new Token(ChainId.MAINNET, '0x2170Ed0880ac9A755fd29B2688956BD959F933F8', 18,  'ETH', 'Binance-Peg Ethereum Token')
+export const APBK = new Token(ChainId.MAINNET, '0x3705496eE4614f7fE939E410f5A03dF988148e50', 9,  'APBK', 'ApeBlack')
 
 const WBNB_ONLY: ChainTokenList = {
   [ChainId.MAINNET]: [WETH[ChainId.MAINNET]],
@@ -119,7 +115,8 @@ export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
     USDT,
     BTCB,
     ARTH,
-    DUET
+    DUET,
+    APBK
   ]
 }
 
@@ -134,19 +131,19 @@ export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: To
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES: ChainTokenList = {
   ...WBNB_ONLY,
-  [ChainId.MAINNET]: [CZUSD, CZF, KYIV, DAMP, LRT]
+  [ChainId.MAINNET]: [APBK]
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
   ...WBNB_ONLY,
-  [ChainId.MAINNET]: [...WBNB_ONLY[ChainId.MAINNET], CZUSD, CZF, BUSD]
+  [ChainId.MAINNET]: [...WBNB_ONLY[ChainId.MAINNET], APBK, BUSD]
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
   [ChainId.MAINNET]: [
-    [CZUSD, BUSD],
-    [CZUSD, CZF]
+    [APBK, BUSD],
+    [APBK, BTCB]
   ]
 }
 
